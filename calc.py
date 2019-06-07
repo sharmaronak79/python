0420f06750eb100a2812d7651c9e0b0aee505f4a
Ronak Sharma <sharmaronak79@yahoo.com>
2019-06-06 21:33:53 -0400

    calc.py

---
+++ calc.py
@@ -0,0 +1,6 @@
+print(4/2) #float divison
+print(4//2)
+print(2//4)
+print(2**3)
+print(2**0.5)
+print(round(2**0.5,4))
---
+++ int_func.py
@@ -0,0 +1,4 @@
+number_one=int(input("enter first number\n"))
+number_two=int(input("enter secnd number\n"))
+total=number_one + number_two   
+print("total is "+ str(total)) 
