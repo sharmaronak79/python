d57d3ffb4648519951192a14c60eec72aebc7f28
ronak <sharmaronak79@yahoo.com>
2019-09-12 21:57:25 -0400

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# user input
+# input function
+name=input("what is your name?\n")
+print("hello" +" "+ name)
+age = input("Mr." + name +" "+ " please enter your age\n")
+print(name+"'s" + " age is"+ " "+age)
