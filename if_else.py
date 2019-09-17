ce945ee4e0356f23a5fdccd7fd76b18c106864ad
ronak <sharmaronak79@yahoo.com>
2019-09-16 22:38:08 -0400

    if_else.py

---
+++ if_else.py
@@ -0,0 +1,9 @@
+# tutorial 39, 40, 41;
+# if else statement and pass statement
+
+age= input("entr your age")
+age=int(age)
+if age>=15:
+    print("you can play")
+else :
+    print("sorry , you cant play for now")    
