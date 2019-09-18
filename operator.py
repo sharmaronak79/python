4e480a2f819a7f36f67616066e3ea98cd4d7a2aa
ronak <sharmaronak79@yahoo.com>
2019-09-17 22:26:44 -0400

    operator.py

---
+++ operator.py
@@ -0,0 +1,12 @@
+#tutorial 44 & exercises related to it
+#
+
+name = input("enter your name: ")
+age = input("enter yuour age: ")
+age = int(age)
+
+if age >= 10 and (name[0]=='a' or name[0]=='A'):
+    print("you can wath a movie")
+else:
+    print("sorry , you cannot watch a movie")    
+
