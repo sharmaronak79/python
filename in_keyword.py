07ffab1c0a976603b2be3b49428047803b4f86ca
ronak <sharmaronak79@yahoo.com>
2019-09-24 22:13:48 -0400

    in_keyword.py

--- in_keyword.py
+++ in_keyword.py
@@ -1,10 +1,29 @@
-# tutorial 48
-#in key word in if statement
+# tutorial 48, 49
 
-name = input("enter any string : ")
-char = input("enter a character that you want to check : ")
 
-if char in name:
-    print(" ",char," is available in this given string ",name,"  ")
+# 1. in key word in if statement
+
+#name = input("enter any string : ")
+#char = input("enter a character that you want to check : ")
+
+#if char in name:
+ #   print(" ",char," is available in this given string ",name,"  ")
+#else:
+ #   print("there is no such ",char," in this string ")    
+#1
+
+##################################################################
+#2. whether it is empty or not
+
+name=input("enter your name: ")
+
+if name :
+
+        print(f"you enter {name}")
+
 else:
-    print("there is no such ",char," in this string ")    
+            print("please enter your name, you did not enter any thing")
+
+
+
+    
