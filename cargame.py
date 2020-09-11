5418a67299ee89322abda686a8c672bb0d167227
ronak <sharmaronak79@yahoo.com>
2020-09-11 15:39:47 -0400

    cargame.py

---
+++ cargame.py
@@ -0,0 +1,29 @@
+
+print("welcome to car game ")
+started = False
+command = ""
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("car is already stared...")
+        else: 
+            started = True   
+            print("car is started...")
+    elif command == "stop":
+        if not started:
+            print("car is already stopped...")
+        else:
+            started = False
+            print("car is stopped.")
+
+    elif command == "help":
+        print("""
+        start - to start a car
+        stop - to stop a car
+        quit - to quit a game        
+            """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry...I don't understand..")
