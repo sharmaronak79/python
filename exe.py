9735a3b6426c502038d27820e448bc458ada38fc
ronak <sharmaronak79@yahoo.com>
2020-09-11 14:42:25 -0400

    exe.py

--- exe.py
+++ exe.py
@@ -1,11 +1,17 @@
-a="Ronak"
-print(a.lower())
+#Initialize array     
+arr = [25, 11, 7, 79, 56,65,80]     
+     
+max = arr[0]
+     
+for i in arr:      
+   if i > max:    
+       max = i    
+           
+print(f"Largest element present in given array:{max} " ) 
 
-print(a.upper())
-
-b=[]
-b.append(25)
-b.append(30)
-b.append("jwal")
-b.append("ronak")
-print(b)
+numbers = [10,20,30,40,50,21,25]
+large = numbers[0]
+for num in numbers:
+    if num > large:
+        large=num
+print(f"second large number is : {large}")
