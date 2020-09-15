64d16057bcf3d6c438327b4457443c28fc97d5c9
ronak <sharmaronak79@yahoo.com>
2020-09-15 17:38:02 -0400

    function_parameter_argument.py

---
+++ function_parameter_argument.py
@@ -0,0 +1,7 @@
+def user_greet(first_name,last_name):
+    print(f"hi {first_name} {last_name}")
+    print("god luck for game")
+
+first_name= input("hi, what is first name? : ")
+last_name= input("hi, what is last name? : ")
+user_greet(first_name , last_name)
