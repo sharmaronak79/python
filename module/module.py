4c074d739b028a668a5b09705468f876a159cce6
ronak <sharmaronak79@yahoo.com>
2020-09-18 19:14:20 -0400

    module.py

--- module/module.py
+++ module/module.py
@@ -3,8 +3,10 @@
 # here i have creatred some function of converters in conveter.py and imort them to my module file 
 # so it like header file we create in c language and include it in our surc code
 #here the key words are used are import
-
 import converter
 converter.kg_to_lbs()
 from converter import lbs_to_kg
 lbs_to_kg()
+s=[10,55,30,70,44]
+from find_max import find_max
+find_max(s)
