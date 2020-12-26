05617044752564efb41aea79df2af3fb1597d03b
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-12-25 23:39:38 -0500

    Update average.py

--- average.py
+++ average.py
@@ -1,3 +1,3 @@
-n1,n2,n3=input("enter three number").split()
+n1,n2,n3=input("enter  three number").split()
 avg=round((int(n1)+int(n2)+int(n3))/3,4) # to limit the number after pointthe round is used and upto howmany number u want after poin is written after comma tutorial 14  
 print(f"average of three numbers is : {avg}")
