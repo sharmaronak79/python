d0748ebe359554cffb50a36da635fb210111011a
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-10-24 14:30:20 -0400

    Update scpi.py

--- WorkPlace/scpi.py
+++ WorkPlace/scpi.py
@@ -41,9 +41,7 @@ def canary_pyvisa():
     canary = rm.open_resource('TCPIP0::192.168.0.30::5555::SOCKET')
     canary.write_termination = '\n'
     canary.read_termination = '\n'
-    time.sleep(0.1)
-
-
+    time.sleep(1)
 
     # Below code is useful to check the system status  ON/OFF
     l1 = canary.read()
@@ -62,42 +60,78 @@ def canary_pyvisa():
     print(data)
 
     # :rfx:status
-    canary.write(":rfall:status")
+    canary.write(":rfall:status?")
     rfx_channel_status = canary.read()
     print(rfx_channel_status)
     var.AVAILABLE_MODULES = int(list(filter(str.isdigit, rfx_channel_status))[0])
     print('Available_modules : {0}'.format(var.AVAILABLE_MODULES))
+    i = var.AVAILABLE_MODULES
+    while i > 0:
+        print(canary.read())
+        i -= 1
+
 
     # :rfx:temp - This is a command to check temperature of particular RFModule where x is 1 to 12
     # here query return only the first line (till terminator) of buffer so used additional 3 read() to get all U3,U4 and U5 temperature
-    canary.write(":rf2:temp")
-    i = 4
-    while i > 0:
-        print(canary.read())
-        i -= 1
+    # canary.write(":rf3:temp")
+    # i = 4
+    # while i > 0:
+    #     print(canary.read())
+    #     i -= 1
 
     # :rfall:temp, Here the value of i will be number of available RF modules * 4, if you want to see 6 modules temperature then i = 6*4 =24
-    canary.write(":rfall:temp")
-    i = (4 * var.AVAILABLE_MODULES) + 1
+    canary.write(":rfall:temperature?")
+    i = (4 * var.AVAILABLE_MODULES)
     while i > 0:
         print(canary.read())
         i -= 1
 
-    # print(canary.query(":rfall:freq 10123 -44.44"))
-    # print(canary.query(":rf2:status"))
 
-    # print(canary.query(":syst:configuration 1"))
-
-    # print(canary.query(":system:lmktemperature?"))
-    # print(canary.write(":syste:temp?"))
-    # print(canary.read())
-    # print(canary.read())
+    canary.write(":syste:temp?")
+    print(canary.read())
+    print(canary.read())
+    print(canary.read())
     # print(canary.read())
 
-    lmk_tmp = canary.query(":system:lmktemperature?")
-    print(lmk_tmp)
-    lmk_tmp = list(lmk_tmp.split(" "))
+
+    # lmk_tmp = canary.query(":system:lmktemperature?")
     # print(lmk_tmp)
-    var.LMK_TEMP = lmk_tmp[4]
-    print(float(var.LMK_TEMP))
+    # lmk_tmp = list(lmk_tmp.split(" "))
+    # print(lmk_tmp)
+    # var.LMK_TEMP = lmk_tmp[4]
+    # print(float(var.LMK_TEMP))
 
+    rm.close()
+
+
+def set_configuration():
+    rm = pyvisa.ResourceManager()
+    canary = rm.open_resource('TCPIP0::192.168.0.30::5555::SOCKET')
+    canary.write_termination = '\n'
+    canary.read_termination = '\n'
+    time.sleep(0.1)
+
+    canary.write(":system:configuration 2")
+    time.sleep(2)
+    canary.read()
+
+    print('Configuration is set')
+    rm.close()
+
+
+def set_freq():
+    rm = pyvisa.ResourceManager()
+    canary = rm.open_resource('TCPIP0::192.168.0.30::5555::SOCKET')
+    canary.write_termination = '\n'
+    canary.read_termination = '\n'
+    time.sleep(0.1)
+
+    data = canary.query('*IDN?')
+    print(data)
+
+    canary.write(":rf3:freq 8000 20")
+    time.sleep(3)
+    canary.read()
+
+    print('Frequency is set')
+    rm.close()
