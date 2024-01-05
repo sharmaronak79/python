31801228abf31f0ae65ca18efaf3a3cefeb4780e
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2024-01-05 15:57:57 -0500

    Update Cubeprogrammer.py

--- GUI_Automation/Cubeprogrammer.py
+++ GUI_Automation/Cubeprogrammer.py
@@ -5,6 +5,25 @@ from pywinauto.keyboard import send_keys
 def cubeprogrammer():
     print("Opening STM32 CubProgrammer........")
 
-    # Application(backend='uia').start(cmd_line=r"C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32CubeProgrammer.exe").connect(title="STM32CubeProgrammer")
-    app = Application(backend='uia').connect(title='STM32CubeProgrammer')
-    
+    app = Application(backend='uia').start(cmd_line=r"C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32CubeProgrammer.exe",timeout=20)
+    time.sleep(13)
+
+    app.connect(title='STM32CubeProgrammer')
+    time.sleep(2)
+
+    main_window = app.window(title_re = 'STM32CubeProgrammer')
+
+    ConnectButton = main_window.child_window(title="Connect", auto_id="JavaFX184", control_type="Button").wrapper_object()
+    ConnectButton.click()
+
+    OpenfileButton = main_window.child_window(title="Open file", auto_id="JavaFX109", control_type="Button").wrapper_object()
+    # OpenfileButton.click()
+    print('Open file clicked')
+
+    Openfile_window = app.window(title_re = 'Open file')
+    Openfile_window.print_control_identifiers()
+    time.sleep(5)
+
+    CloseButton = main_window.child_window(title="Close", control_type="Button").wrapper_object()
+    CloseButton.click()
+
