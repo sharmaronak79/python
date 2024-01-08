d23ac557907b88b5656658e7ddbc4f99a4d20201
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2024-01-08 11:37:17 -0500

    Create STM32CubeProgrammer_Final.py

---
+++ GUI_Automation/STM32CubeProgrammer_Final.py
@@ -0,0 +1,39 @@
+import pywinauto
+import time
+from pywinauto.application import Application
+from pywinauto.keyboard import send_keys
+
+def cubeprogrammer():
+    print("Opening STM32 CubProgrammer........")
+
+    app = Application(backend='uia').start(cmd_line=r"C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32CubeProgrammer.exe",timeout=20)
+    time.sleep(10)
+
+    app.connect(title='STM32CubeProgrammer')
+    time.sleep(1)
+
+    main_window = app.window(title_re = 'STM32CubeProgrammer')
+
+    #Click on Connect Button
+    main_window.child_window(title="Connect", auto_id="JavaFX184", control_type="Button").click()
+
+    #Click on Memory and File Editing
+    main_window.child_window(auto_id="JavaFX23", control_type="Button").click()
+    time.sleep(1)
+
+    #Click on Erasing and Programming
+    main_window.child_window(title="Erasing & programming", auto_id="JavaFX380", control_type="Button").click()
+    time.sleep(3)
+
+    main_window.child_window(auto_id="JavaFX644", control_type="ComboBox").click_input() #this will click on Edit box
+    send_keys("^a")  # Press CTRL + a
+    send_keys("{VK_DELETE}") # Press Delete button
+    main_window[u'ComboBox:Edit'].type_keys(r"C:\Users\ronakkumar_sharma\Desktop\Boot_and_App\Bootloader.hex") # Write a file path
+    main_window.StartProgramming.click() # Click on Start Programming
+    time.sleep(5) # wait to complete the download
+    send_keys("{ENTER}") # once it is done , Hit the Enter
+    # main_window.print_control_identifiers()
+
+    # Close the Application
+    main_window.child_window(title="Close", control_type="Button").click()
+    # app.kill()
